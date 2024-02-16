4be9f4eb9bc1abd5599665be06ed01c8d129bed3
Ramiro Ferraresi <ramiro_ferraresi@hotmail.com>
2024-02-16 11:42:59 -0300

    toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <App />
-     </ThemeProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      <Toaster />
+    </ThemeProvider>
   </BrowserRouter>
 );
